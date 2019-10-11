ec6f05ca174c18115ecc35bb702840d0c744f9e5
ksementkovskaya <k.sementkovskaya@yandex.ru>
2019-10-11 17:54:29 +0300

    настроила форму обратной связи

---
+++ js/popup.js
@@ -0,0 +1,63 @@
+var link = document.querySelector(".write-to-us");
+
+var popup = document.querySelector(".modal-feedback-form");
+
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+
+var fullname = popup.querySelector("[name=fullname]");
+
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+
+var storage = "";
+
+try {
+  storage = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+
+  if (storage) {
+    email.value = storage;
+    fullname.focus();
+  } else {
+    email.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!fullname.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("email", email.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
